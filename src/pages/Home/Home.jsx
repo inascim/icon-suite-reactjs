78dc50dcff6a2bcab3be650d17fb8fdce4451a2e
Igor Nascimento <igor.f.nascimento@accenture.com>
2022-08-20 23:54:50 -0300

    dev: ajustando quebra de tela, amanha incluo modais de criacao de novo jogo, bem como modal de login sem uso ainda

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,27 +7,30 @@ import { Grid } from "@mui/material";
 
 export default function Home({debug = false}) {
     const theme = useTheme();
-    const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+    const fullScreenDown = useMediaQuery(theme.breakpoints.down('sm'));
+    const fullScreenUp = useMediaQuery(theme.breakpoints.up('sm'));
 
     useEffect(() => {
-        if (fullScreen) {
+        if (fullScreenUp || fullScreenDown) {
             console.log('quebrou a tela')
         }
-    }, [fullScreen]);
+    }, [fullScreenDown, fullScreenUp]);
     
 
     return (
         <Grid container item flexGrow={1} backgroundColor={debug ? 'red' : null} p={2}>
-            <Grid container item xs={6} px={2}>
+            <Grid container item xs={12} sm={6} px={2}>
                 <GamesTableContainer />
             </Grid>
-            <Grid container item xs={4} backgroundColor={debug ? 'yellow' : null} px={2}
-                direction={'column'}>
-                <Grid item xs={6} backgroundColor={debug ? 'white' : null}>
-                    {/* Login Card  */}
-                    <LoginCard />
+            {fullScreenUp && (
+                <Grid container item xs={4} backgroundColor={debug ? 'yellow' : null} px={2}
+                    direction={'column'}>
+                    <Grid item xs={6} backgroundColor={debug ? 'white' : null}>
+                        {/* Login Card  */}
+                        <LoginCard />
+                    </Grid>
                 </Grid>
-            </Grid>
+            )}
         </Grid>
     )
 }
